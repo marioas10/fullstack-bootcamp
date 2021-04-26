5e13655ca0db16047f8f09d4abed1cd6707e62b1
MarioAS <43919016+marioas10@users.noreply.github.com>
2021-04-26 21:01:13 +0100

    excercises 1.1.-1.2. finished

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,17 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+/* 
+DESESTRUCTURACIÓN (Extraer propiedad de un objeto directamente)
+ 
+const Title = ({course}) => <h1>{course}</h1>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const {course} = props
+const course = props.course
+ */
+
+const Header = (props) => {
+  return <h1>{props.course.name}</h1>
+}
+
+const Content = (props) => {
+  const { parts } = props.data;
+
+  return (
+    <div>
+      <Part part={parts[0].name} exercises={parts[0].exercises} />
+      <Part part={parts[1].name} exercises={parts[1].exercises} />
+      <Part part={parts[2].name} exercises={parts[2].exercises} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return <p>{props.part} {props.exercises}</p>
+}
+
+const Total = (props) => {
+  const { parts } = props.data;
+  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
+
+  return <p>Number of exercises {total}</p>
+}
+
+const App = () => {
+  /* const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14 */
+
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content data={course} />
+      <Total data={course} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
